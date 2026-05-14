a8d31653c1d180a557599d9db4a813854937a0a1
klacruz <24-10528@usb.ve>
2026-05-14 18:56:27 -0400

    Update ex.cxx

--- Tarea1/ex.cxx
+++ Tarea1/ex.cxx
@@ -7,14 +7,41 @@ int main(){
 }
 
 double mean(vector<double> v){
+  if (v.empty()) return 0.0;
+    double sum = accumulate(v.begin(), v.end(), 0.0);
+    return sum / v.size();
   return 0;
 }
 
 double variance(vector<double> v){
+  if (v.size() < 2) return 0.0;
+    double m = mean(v);
+    double accum = 0.0;
+    for (double x : v) {
+        accum += (x - m) * (x - m);
   return 0;
 }
 
 double pearson_r(vector<double> A, vector<double> B){
+  if (A.size() != B.size() || A.empty()) return 0.0;
+
+    int n = A.size();
+    double sum_A = accumulate(A.begin(), A.end(), 0.0);
+    double sum_B = accumulate(B.begin(), B.end(), 0.0);
+    
+    double mean_A = sum_A / n;
+    double mean_B = sum_B / n;
+
+    double num = 0.0;
+    double den_A = 0.0;
+    double den_B = 0.0;
+
+    for (int i = 0; i < n; ++i) {
+        double diff_A = A[i] - mean_A;
+        double diff_B = B[i] - mean_B;
+        num += diff_A * diff_B;
+        den_A += diff_A * diff_A;
+        den_B += diff_B * diff_B;
   return 0;
 }
 
