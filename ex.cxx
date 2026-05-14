cbcf444f8de1afce857c3bfbe402f4203f9b5bff
klacruz <24-10528@usb.ve>
2026-05-14 12:53:19 -0400

    Add files via upload

---
+++ ex.cxx
@@ -0,0 +1,32 @@
+#include <vector>
+
+using namespace std;
+
+int main(){
+  return 0;
+}
+
+double mean(vector<double> v){
+  return 0;
+}
+
+double variance(vector<double> v){
+  return 0;
+}
+
+double pearson_r(vector<double> A, vector<double> B){
+  return 0;
+}
+
+vector<char> dec_to_septapus(int n){return {};}
+vector<char> dec_to_octopus(int n){return {};}
+vector<char> dec_to_hexakaidecapus(int n){return {};}
+vector<char> septapus_to_dec(vector<char> s){return {};}
+vector<char> octopus_to_dec(vector<char> s){return {};}
+vector<char> hexakaidecapus_to_dec(vector<char> s){return {};}
+vector<char> septapus_to_octopus(vector<char> s){return {};}
+vector<char> septapus_to_hexakaidecapus(vector<char> s){return {};}
+vector<char> octapus_to_septapus(vector<char> s){return {};}
+vector<char> octopus_to_hexakaidecapus(vector<char> s){return {};}
+vector<char> hexakaidecapus_to_septapus(vector<char> s){return {};}
+vector<char> hexakaidecapus_to_octopus(vector<char> s){return {};}
